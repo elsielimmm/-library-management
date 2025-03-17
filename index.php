b08a205bdecb08823fb3fea4b11209a6ae23d65b
Phuong Pham <101460098+elsielimmm@users.noreply.github.com>
2025-03-17 21:59:37 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Project File Management System</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel = "stylesheet" type="text/css" href="admin/css/bootstrap.css" />
+		<link rel = "stylesheet" type="text/css" href="admin/css/style.css" />
+		<style>
+
+	#footer {
+            text-align: center; /* Căn giữa nội dung theo chiều ngang */
+        }
+</style>
+	</head>
+<body>
+	<nav class="navbar navbar-default navbar-fixed-top navbar-inverse">
+		<div class="container-fluid">
+			<label class="navbar-brand">Project File Management System</label>
+		</div>
+	</nav>
+	<?php include 'mvc/view/login.php'?>
+	<div id = "footer">
+		<label class = "footer-title">  An Nguyen & Phuong Pham </label>
+	</div>
+</body>
+</html>
